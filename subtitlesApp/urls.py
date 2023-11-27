9bfc51cb0050ccc7c80593bcecc011092caa9340
Sanket Ugale <sanketugale683@outlook.com>
2023-11-28 03:00:24 +0530

    Commit

--- subtitlesApp/urls.py
+++ subtitlesApp/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import process_video
 from subtitlesApp import views
 
 urlpatterns = [
-    path('process_video/', views.process_video, name='process_video'),
+    # path('process_video/', views.process_video, name='process_video'),
     # Add other URL patterns as needed
     path('login/', views.login),
-    path('', views.home),
+    path('', views.home, name='home'),
 ]
